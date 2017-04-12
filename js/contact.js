120b04288cc026b9222bc5550b57bb4f3b45bca3
Yurii Puzikov <sai_puzikov@hotmail.com>
2017-04-12 14:44:27 +0000

    Final review

--- js/contact.js
+++ js/contact.js
@@ -5,19 +5,21 @@ $(function() {
 
 
 function sendForm(e) {
-    e.preventDefault();
-    $.ajax({
-        url: "https://formspree.io/sai_puzikov@hotmail.com", 
-        method: "POST",
-        data: {
-          name: $('#name').val(),
-          email: $('#email').val(),
-          message: $('#message').val()
-        },
-        dataType: "json"
-    })
-    .done(function() { 
-      $('.form').html('<img src="img/okey.png" alt="ok" id="okey"> <p>Received!</p>');
-    });
-
+  var form = document.querySelector('form');
+  if (form.checkValidity()) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/sai_puzikov@hotmail.com", 
+            method: "POST",
+            data: {
+              name: $('#name').val(),
+              email: $('#email').val(),
+              message: $('#message').val()
+            },
+            dataType: "json"
+        })
+        .done(function() { 
+          $('.form').html('<img src="img/okey.png" alt="ok" id="okey"> <p>Received!</p>');
+        });
+  }
 }
